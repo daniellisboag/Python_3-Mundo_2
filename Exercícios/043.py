36b35dac66b935753e019bd1be0ff6c3314bdc97
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-10 16:50:40 -0300

    Resolução do exercício 043

--- Exercícios/043.py
+++ Exercícios/043.py
@@ -3,4 +3,20 @@
 # Entre 18.5 e 25: Peso ideal.
 # 25 até 30: Sobrepeso.
 # 30 até 40: Obesidade.
-# Acima de 40: Obesidade mórvida.
+# Acima de 40: Obesidade mórbida.
+
+peso = float(input('Qual é o seu peso? (Kg) '))
+altura = float(input('Qual é a sua altura? (m) '))
+imc = peso / (altura ** 2)
+
+print(f'Seu peso é {peso:.2f}Kg e sua altura é {altura:.2f}. O índice de massa corporal é {imc:.1f}')
+if imc < 18.5:
+   print('Classificação: Abaixo do peso!')
+elif imc >= 18.5 and imc < 25:
+   print('Classificação: Peso ideal.')
+elif imc >= 25 and imc < 30:
+   print('Classificação: Sobrepeso!')
+elif imc >= 30 and imc < 40:
+   print('Classificação: Obesidade!')
+elif imc >= 40:
+   print('Classificação: Obesidade mórbida.')

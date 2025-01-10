a0f62da3b5e7f7cee9264d6d09c5e78405f200bf
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-10 14:36:35 -0300

    Resolução do exercício 037

--- Exercícios/037.py
+++ Exercícios/037.py
@@ -2,3 +2,19 @@
 # 1 Para binário.
 # 2 Para Octal.
 # 3 Para hexadecimal.
+
+numero = int(input('Digite um número inteiro: '))
+print('Escolha uma das conversões abaixo:')
+print('[1] Binário.')
+print('[2] Octal.')
+print('[3] Hexadecimal.')
+conversao = int(input('Qual será a base para conversão? '))
+
+if conversao == 1:
+   print(f'O número {numero} convertido para binário é {bin(numero)[2:]}')
+elif conversao == 2:
+   print(f'O número {numero} convertido para octal é {oct(numero)[2:]}')
+elif conversao == 3:
+   print(f'O número {numero} convertido para hexadecimal é {hex(numero)[2:]}')
+else:
+   print('Opção inválida. Tente novamente!')

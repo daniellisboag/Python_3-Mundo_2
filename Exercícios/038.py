c4cfdaeb2bbb2ea0b7218b44c34a8fd013e66e6c
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-10 14:45:32 -0300

    Resolução do exercício 038

--- Exercícios/038.py
+++ Exercícios/038.py
@@ -2,3 +2,13 @@
 # O primeiro valor é maior.
 # O segundo valor é maior.
 # Não existe valor maior, os dois são iguais.
+
+primeiro = int(input('Primeiro número: '))
+segundo = int(input('Segundo número: '))
+
+if primeiro > segundo:
+   print(f'O primeiro valor: {primeiro} é maior que o segundo valor: {segundo}')
+elif segundo > primeiro:
+   print(f'O segundo valor: {segundo} é maior que o primeiro valor: {primeiro}')
+elif primeiro == segundo:
+   print(f'Não existe valor maior.\nO primeiro valor: {primeiro} é igual ao segundo valor: {segundo}')

f076f43db3216a2e5a9eddbca1e48e31f40b495f
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-10 15:31:51 -0300

    Resolução do exercício 040

--- Exercícios/040.py
+++ Exercícios/040.py
@@ -2,3 +2,14 @@
 # Média abaixo de 5.0: Reprovado.
 # Média entre 5.0 e 6.9: Recuperação.
 # Média 7.0 ou superior: Aprovado.
+
+primeira = float(input('Digite a primeira nota: '))
+segunda = float(input('Digite a segunda nota: '))
+media = (primeira + segunda) / 2
+
+if media < 5.0:
+   print(f'A média entre {primeira :.1f} e {segunda :.1f} é igual a {media :.1f} e foi reprovado.')
+elif media >= 5.0 and media < 7.0:
+   print(f'A média entre {primeira :.1f} e {segunda :.1f} é igual a {media :.1f} e precisa de recuperação.')
+elif media >= 7.0:
+   print(f'A média entre {primeira :.1f} e {segunda :.1f} é igual a {media :.1f} e foi aprovado.')

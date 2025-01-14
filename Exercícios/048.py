39b92e8c7938da4b4a6ccac5d853af0242a08667
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-14 09:56:12 -0300

    Resolução do exercício 048

--- Exercícios/048.py
+++ Exercícios/048.py
@@ -1,2 +1,11 @@
 # Faça um programa que calcule todos os números ímpares que são múltiplos
 # de três e que se escontram no intervalo 1 até 500.
+
+soma = 0
+valores = 0
+for contagem in range(1, 501):
+   if contagem % 2 == 1:
+      if contagem % 3 == 0:
+         valores = valores + 1
+         soma = soma + contagem
+print(f'A soma de todos os {valores} valores solicitados é {soma}')

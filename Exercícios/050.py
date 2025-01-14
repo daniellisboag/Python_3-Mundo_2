152d830c37f3463502ba18a086013a16407a26f0
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-14 10:18:19 -0300

    Resolução do exercício 050

--- Exercícios/050.py
+++ Exercícios/050.py
@@ -1,2 +1,11 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas
 # daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+
+soma = 0
+conta = 0
+for contagem in range(1, 7):
+   numero = int(input(f'Digite o {contagem}° número: '))
+   if numero % 2 == 0:
+      soma = soma + numero
+      conta = conta + 1
+print(f'Você informou {conta} números pares e a soma foi {soma}')

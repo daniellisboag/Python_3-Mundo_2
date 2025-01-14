7377dbb4fde244659bff21e80f38c0e04b4c7bf4
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-14 09:37:43 -0300

    Resolução do exercício 047

--- Exercícios/047.py
+++ Exercícios/047.py
@@ -1,1 +1,11 @@
 # Crie um programa que mostre na tela todos o números pares que estão no intervalo entre 1 e 50.
+
+for contagem in range(1, 51):
+   if contagem % 2 == 0:
+      print(contagem, end = ', ')
+print('Fim!')
+
+# Ou
+for contagem in range(2, 51, 2):
+   print(contagem, end = ', ')
+print('Fim!')

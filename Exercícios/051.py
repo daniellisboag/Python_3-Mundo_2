bc3f498ff0fa795243f176063597448ef6b03fe1
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-14 10:42:33 -0300

    Resolução do exercício 051

--- Exercícios/051.py
+++ Exercícios/051.py
@@ -1,2 +1,13 @@
 # Desenvolva um programa que leia o primeiro termo e a razão de um PA.
 # No final, mostre os 10 primeiros termos dessa progressão.
+
+print('=' * 30)
+print('10 termos de uma PA'.center(30))
+print('=' * 30)
+
+termo = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+
+for c in range(termo, (termo + (10 - 1) * razao) + razao, razao):
+   print(f' {c} -> ', end = '')
+print('Fim!')

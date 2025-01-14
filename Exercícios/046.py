153974da6696360643d420369ec152d8cf0242cb
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-14 09:27:58 -0300

    Resolução do exercício 046

--- Exercícios/046.py
+++ Exercícios/046.py
@@ -1,2 +1,10 @@
 # Faça um programa que mostre na tela uma contagem regressiva para o estouro
 # de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+from time import sleep
+from emoji import emojize
+
+for contagem in range(10, -1, -1):
+   sleep(1)
+   print(contagem)
+print(emojize(f'Bum! Bum! Pooow! {':party_popper: ' * 3}'))

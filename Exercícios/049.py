ca671e690cad1313a3615bd97223b24a0861703d
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-14 10:03:15 -0300

    Resolução do exercício 049

--- Exercícios/049.py
+++ Exercícios/049.py
@@ -1,2 +1,7 @@
 # Refaça o desafio 009, mostrando a tabuada de um número que o
 # usuário escolher, só que agora utilizando um laço for.
+
+numero = int(input('Digite um número para ver sua tabuada: '))
+
+for contagem in range(1, 11):
+   print(f'{numero} x {contagem} = {numero * contagem}')

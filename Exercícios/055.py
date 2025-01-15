ffd398411442bc80e6ff2c59b386733f88af1c63
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-15 11:31:25 -0300

    Resolução do exercício 055

--- Exercícios/055.py
+++ Exercícios/055.py
@@ -1,1 +1,14 @@
 # Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menos peso lidos.
+
+maior = 0
+menor = float('inf')
+
+for contagem in range(1, 6):
+   peso = float(input(f'Digite o peso da {contagem}° pessoa: '))
+   if peso > maior:
+      maior = peso
+   if peso < menor:
+      menor = peso
+   
+print(f'O maior peso foi {maior}')
+print(f'O menor peso foi {menor}')

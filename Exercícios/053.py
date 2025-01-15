3ab409eec74e149490d6a2a1f7f4cd140b81b06d
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-15 10:53:02 -0300

    Resolução do exercício 053

--- Exercícios/053.py
+++ Exercícios/053.py
@@ -1,1 +1,25 @@
-# Crie um programa que leia uma frase qualquer e diga se ela é um palíndroma, desconsiderando os espaços.
+# Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+
+frase = input('Digite uma frase: ').strip().split()
+inverter = ''.join(frase).upper()
+
+print(f'O inverso de {inverter} é {inverter[::-1]}')
+
+if inverter == inverter[::-1]:
+   print('Temos um palíndromo!')
+else:
+   print('A frase digitada não é um palíndromo!')
+
+#Ou
+frase = input('Digite uma frase: ').strip().split()
+juntar = ''.join(frase).upper()
+inverter = ''
+
+for inverso in range(len(juntar) - 1, -1, -1):
+   inverter += juntar[inverso]
+print(f'O inverso de {juntar} é {inverter}')
+
+if inverter == juntar:
+   print('Temos um palíndromo!')
+else:
+   print('A frase digitada não é um palíndromo!')

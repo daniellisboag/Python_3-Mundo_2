96e201667af2ecf82fec513c9313d6e294fe967e
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-15 10:28:10 -0300

    Resolução do exercício 052

--- Exercícios/052.py
+++ Exercícios/052.py
@@ -1,1 +1,18 @@
 # Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+
+numero = int(input('Digite um número: '))
+soma = 0
+
+for contagem in range(1, numero + 1):
+   if numero % contagem == 0:
+      print('\033[34m', end = ' ')
+      soma += 1
+   else:
+      print('\033[m', end = ' ')
+   print(f'{contagem}\033[m', end = ' ')
+print(f'\nO número {numero} foi divisível {soma} vezes.')
+
+if soma == 2:
+   print('É um número primo.')
+else:
+   print('Não é um número primo!')

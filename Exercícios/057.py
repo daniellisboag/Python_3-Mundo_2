f6d9aa2d359822122a29a84932bbd6c65146c652
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-16 11:06:21 -0300

    Resolução do exercício 057

--- Exercícios/057.py
+++ Exercícios/057.py
@@ -1,2 +1,23 @@
 # Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'.
 # Caso esteja errado, peça a digitação novamente até ter um valor correto.
+
+sexo = input('Digite seu sexo: [M/F] ')
+
+while sexo.upper() != 'M' and sexo.upper() != 'F':
+   sexo = input('Dados inválidos. Por favor, informe seu sexo: ')
+
+if sexo.upper() == 'M':
+   print('Sexo masculino registrado com sucesso.')
+elif sexo.upper() == 'F':
+   print('Sexo feminino registrado com sucesso.')
+
+# Ou
+sexo = input('Informe seu sexo: [M/F] ').strip()[0]
+
+while sexo not in 'MmFf':
+   sexo = input('Dados inválidos. Por favor informe seu sexo: ').strip()[0]
+
+if sexo.upper() == 'M':
+   print('Sexo masculino registrado com sucesso.')
+elif sexo.upper() == 'F':
+   print('Sexo feminino registrado com sucesso.')

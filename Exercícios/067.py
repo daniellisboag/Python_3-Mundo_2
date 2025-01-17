4da5ab9ecd59389f5b65d3f1eb59298c9fd2f72e
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-17 17:03:06 -0300

    Exercícios do 066 ao 072

---
+++ Exercícios/067.py
@@ -0,0 +1,2 @@
+# Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor
+# digitado pelo usúario. O programa será interrompido quando o número solicitado for negativo.

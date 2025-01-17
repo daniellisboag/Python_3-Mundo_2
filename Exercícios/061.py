5db6e0825b452a98b432c5dd5c7b58ea77301fa8
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-17 10:40:05 -0300

    Resolução do exercício 061

--- Exercícios/061.py
+++ Exercícios/061.py
@@ -1,17 +1,6 @@
 # Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando
 # os 10 primeiros termos da progressão usando a estrutura while.
 
-'''print('=' * 30)
-print('10 termos de uma PA'.center(30))
-print('=' * 30)
-
-termo = int(input('Primeiro termo: '))
-razao = int(input('Razão: '))
-
-for c in range(termo, (termo + (10 - 1) * razao) + razao, razao):
-   print(f' {c} -> ', end = '')
-print('Fim!')'''
-
 print('=' * 30)
 print('10 termos de uma PA'.center(30))
 print('=' * 30)

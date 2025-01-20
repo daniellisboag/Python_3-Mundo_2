f713e32061eaa1fd1cb8129fa745ab1b9dcfd41b
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-01-20 09:07:19 -0300

    Resolução do exercício 067

--- Exercícios/067.py
+++ Exercícios/067.py
@@ -1,2 +1,13 @@
 # Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor
 # digitado pelo usúario. O programa será interrompido quando o número solicitado for negativo.
+
+while True:
+   tabuada = int(input('Quer ver a tabuada de qual valor? '))
+   if tabuada < 0:
+      break
+   print('-' * 40)
+   for c in range(1, 11):
+      print(f'{tabuada} x {c} = {tabuada * c}')
+   print('-' * 40)
+
+print('Programa de tabuada encerrado. Volte sempre!')
